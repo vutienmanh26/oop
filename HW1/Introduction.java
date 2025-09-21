571bad659a35d1071cd602c2d1d9068d5933fb91
oop <24020226@vnu.edu.vn>
2025-09-21 23:24:13 +0700

    fix git.me and introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,17 @@
+public class Introduction {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Vu Tien Manh\t24020226\tIT1\tvutienmanh26\t24020226@vnu.edu.vn");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                            "Take one down, pass it around, \n" +
+                            "8 bottles of beer on the wall, 8 bottles of beer.\n" +
+                            "Take one down, pass it around,\n" +
+                            "...\n" +
+                            "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                            "Take one down, pass it around,\n" +
+                            "No more bottles of beer on the wall.\n");
+    }
+
+}
